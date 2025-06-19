f6e244792c042901251a2ed6a07aefe131b3a0c4
Hamad179 <youremail@example.com>
2025-06-19 23:02:19 +0300

    2

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'clients/home.html')
+    return render(request, 'home.html')
+
+def login_view(request):
+    return render(request, 'clients/login.html')
+
+def register_view(request):
+    return render(request, 'clients/sinup.html')
